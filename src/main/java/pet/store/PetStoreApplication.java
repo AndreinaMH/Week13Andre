444d0cbb2a4a48c4ef678cae6a75af12331c9dd1
AndreinaMH <amartinez.hira@gmail.com>
2024-06-28 22:02:39 -0400

    first commit

---
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,13 @@
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+	public static void main(String[] args) {
+SpringApplication.run(PetStoreApplication.class, args);
+	}
+}
+// password: pet_store
+// USERNAME pet_store
